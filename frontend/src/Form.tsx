7968edf1122d889fe5d75a72b1b988123640f442
Aapeli <git@aapelivuorinen.com>
2021-08-03 15:10:58 +0300

    Add rest of form

--- frontend/src/Form.tsx
+++ frontend/src/Form.tsx
@@ -17,6 +17,16 @@ import classNames from "classnames";
 
 import { makeStyles } from "@material-ui/core";
 
+const YES_NO = [
+  {
+    value: "yes",
+    label: "Yes",
+  },
+  {
+    value: "no",
+    label: "No",
+  },
+];
 const VARIANTS = [
   {
     value: "alpha",
@@ -45,14 +55,14 @@ const SEX_OPTIONS = [
     label: "Female",
   },
 ];
-const YES_NO = [
+const VACCINE_OPTIONS = [
   {
-    value: "yes",
-    label: "Yes",
+    value: "pfizer",
+    label: "Pfizer",
   },
   {
-    value: "no",
-    label: "No",
+    value: "az",
+    label: "AstraZeneca",
   },
 ];
 
@@ -61,12 +71,20 @@ type FormData = {
   age?: number;
   sex: string;
   dose1: string;
+  vaccine: string;
+  dose1weeks?: number;
+  dose2: string;
+  dose2weeks?: number;
+  covid: string;
 };
 
 const useStyles = makeStyles((theme) => ({
   formComp: {
     marginTop: "2rem",
     marginBottom: "2rem",
+  },
+  indent: {
+    paddingLeft: "2rem",
   },
 }));
 
@@ -82,6 +100,11 @@ export default function Form() {
   const classes = useStyles();
 
   const vals = watch();
+
+  const disableDose1extras = watch("dose1") !== "yes";
+  const disableDose2 = disableDose1extras;
+  const disableDose2extras =
+    watch("dose1") !== "yes" || watch("dose2") !== "yes";
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -125,7 +148,16 @@ export default function Form() {
           name="age"
           control={control}
           rules={{ min: 16, max: 100 }}
-          render={({ field }) => <TextField label="Age" {...field} />}
+          render={({ field }) => (
+            <TextField
+              type="number"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              label="Age"
+              {...field}
+            />
+          )}
         />
       </div>
       <div className={classNames(classes.formComp)}>
@@ -168,6 +200,127 @@ export default function Form() {
               <RadioGroup
                 row
                 name="dose1-radio"
+                onChange={(e, value) => onChange(value)}
+                value={value}
+              >
+                {YES_NO.map(({ value, label }) => (
+                  <FormControlLabel
+                    key={label}
+                    value={value}
+                    control={<Radio />}
+                    label={label}
+                  />
+                ))}
+              </RadioGroup>
+            </FormControl>
+          )}
+        />
+      </div>
+      <div className={classNames(classes.formComp, classes.indent)}>
+        <Controller
+          name="vaccine"
+          control={control}
+          defaultValue={""}
+          render={({ field: { onChange, value } }) => (
+            <FormControl component="fieldset">
+              <FormLabel component="legend">Vaccine</FormLabel>
+              <RadioGroup
+                row
+                name="vaccine-radio"
+                onChange={(e, value) => onChange(value)}
+                value={value}
+              >
+                {VACCINE_OPTIONS.map(({ value, label }) => (
+                  <FormControlLabel
+                    disabled={disableDose1extras}
+                    key={label}
+                    value={value}
+                    control={<Radio />}
+                    label={label}
+                  />
+                ))}
+              </RadioGroup>
+            </FormControl>
+          )}
+        />
+      </div>
+      <div className={classNames(classes.formComp, classes.indent)}>
+        <Controller
+          name="dose1weeks"
+          control={control}
+          rules={{ min: 0, max: 104 }}
+          render={({ field }) => (
+            <TextField
+              disabled={disableDose1extras}
+              type="number"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              label="Weeks since first dose"
+              {...field}
+            />
+          )}
+        />
+      </div>
+      <div className={classNames(classes.formComp)}>
+        <Controller
+          name="dose2"
+          control={control}
+          defaultValue={""}
+          render={({ field: { onChange, value } }) => (
+            <FormControl component="fieldset">
+              <FormLabel component="legend">
+                Has had second dose of COVID-19 vaccine?
+              </FormLabel>
+              <RadioGroup
+                row
+                name="dose2-radio"
+                onChange={(e, value) => onChange(value)}
+                value={value}
+              >
+                {YES_NO.map(({ value, label }) => (
+                  <FormControlLabel
+                    disabled={disableDose2}
+                    key={label}
+                    value={value}
+                    control={<Radio />}
+                    label={label}
+                  />
+                ))}
+              </RadioGroup>
+            </FormControl>
+          )}
+        />
+      </div>
+      <div className={classNames(classes.formComp, classes.indent)}>
+        <Controller
+          name="dose2weeks"
+          control={control}
+          rules={{ min: 0, max: 104 }}
+          render={({ field }) => (
+            <TextField
+              disabled={disableDose2extras}
+              type="number"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              label="Weeks since second dose"
+              {...field}
+            />
+          )}
+        />
+      </div>
+      <div className={classNames(classes.formComp)}>
+        <Controller
+          name="covid"
+          control={control}
+          defaultValue={""}
+          render={({ field: { onChange, value } }) => (
+            <FormControl component="fieldset">
+              <FormLabel component="legend">Has had COVID-19?</FormLabel>
+              <RadioGroup
+                row
+                name="covid-radio"
                 onChange={(e, value) => onChange(value)}
                 value={value}
               >
