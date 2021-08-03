9ca0a5e70ea90700f22c87e246eb66517c1a7e12
Aapeli <git@aapelivuorinen.com>
2021-08-03 20:54:24 +0300

    Add prototype note

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { Box, Container, Typography } from "@material-ui/core";
 import Form from "./Form";
 import { FormData, compute } from "./api";
 import Output from "./Output";
+import { Alert, AlertTitle } from "@material-ui/lab/";
 
 export default function App() {
   const [msg, setMsg] = useState("");
@@ -21,6 +22,11 @@ export default function App() {
         <Typography variant="subtitle1" gutterBottom>
           By, credits line, etc.
         </Typography>
+        <Alert severity="error">
+          <AlertTitle>Early prototype!</AlertTitle>
+          This is an early prototype and is under heavy development.
+        </Alert>
+        <p>Debugging info:</p>
         <pre>{msg}</pre>
         <Form callback={callback} />
         <Output output={output} />
