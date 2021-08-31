468bff8e4215fc7c3fc2ed39bd5388e75c127146
Aapeli <git@aapelivuorinen.com>
2021-08-31 00:38:36 -0400

    Try to fix api access

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8888/v0/compute";
+// const API_URL = "http://localhost:8888/v0/compute";
+const API_URL = "https://dev.corical.a19e.net/api/v0/compute";
 
 export type FormData = {
   variant: string;
