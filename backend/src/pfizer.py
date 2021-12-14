2fd521acdc93a156cd909006f627c84bb253e031
Aapeli <git@aapelivuorinen.com>
2021-12-14 17:52:59 -0500

    Fix pfizer calculation given covid 19

--- backend/src/pfizer.py
+++ backend/src/pfizer.py
@@ -22,12 +22,13 @@ def compute_probs(n1_pfizer_dose, n2_age, n4_ct, sex_vec):
     values = {
         "n3_Sex": sex_vec,
     }
-    values_infected = dict(values)
 
     # always hardcode delta
     pfizer.set_fact(values, "n1_Pfizer_dose", n1_pfizer_dose)
     pfizer.set_fact(values, "n2_Age_group", n2_age)
     pfizer.set_fact(values, "n4_Community_transmission", n4_ct)
+
+    values_infected = dict(values)
 
     get_covid = pfizer.infer(values, "n10_Risk_of_infection_under_current_transmission_and_vaccination_status")[0]
     get_myocarditis_vax = pfizer.infer(values, "n5_Vaccine_associated_myocarditis")[0]
@@ -39,7 +40,7 @@ def compute_probs(n1_pfizer_dose, n2_age, n4_ct, sex_vec):
 
     # if infected
     pfizer.set_fact(values_infected, "n10_Risk_of_infection_under_current_transmission_and_vaccination_status", "Yes")
-    die_covid_if_got_it = pfizer.infer(values, "n14_Die_from_COVID19")[0]
+    die_covid_if_got_it = pfizer.infer(values_infected, "n14_Die_from_COVID19")[0]
 
     return (
         get_covid,
