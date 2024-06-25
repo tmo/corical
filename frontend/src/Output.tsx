a22ec0a37660890e0bebd861488f7ec9c6e89a2b
uqhlu8 <hongen.lu@uq.edu.au>
2024-06-25 15:16:45 +1000

    removed exclaimaiton

--- frontend/src/Output.tsx
+++ frontend/src/Output.tsx
@@ -369,7 +369,7 @@ export default function Form({ output }: OutputProps) {
           *The more severe your first week of infection, the higher your chances are of going to hospital or ICU from COVID-19, 
           and the higher your chances of having long COVID symptoms six months after infection.
           <br></br>
-          †To lower your risks even more, you can consider having vaccines before infection AND drug treatment during infection! 
+          †To lower your risks even more, you can consider having vaccines before infection AND drug treatment during infection. 
           For more detail on drug treatment, read our FAQs.
           </Typography>
           </div>
